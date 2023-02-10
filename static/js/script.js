3fc8ae884138eb580e33e9f5bdf892ce0c31f0be
Sajjad Aemmi <sajjadaemmi@gmail.com>
2023-02-10 14:32:20 +0330

    🔨 Modify

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,5 @@
+document.getElementById("year").innerHTML = new Date().getFullYear();
+
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
